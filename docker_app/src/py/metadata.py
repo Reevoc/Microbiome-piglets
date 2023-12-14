53f81e5170e041c7d050ad9d397f687fe7b20b68
Reevoc <61749102+Reevoc@users.noreply.github.com>
2023-12-14 17:28:34 +0100

    commit changes on metadata

--- docker_app/src/py/metadata.py
+++ docker_app/src/py/metadata.py
@@ -3,22 +3,39 @@
 # %% Import LIbaries
 import pandas as pd
 import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 # %% [markdown]
+# ## TODO:
+# - [x] Time is categorical is better if it is numerical
+# Spit in 3 different metadata files for different time: DONE
+# - [x] check for wunded: DONE
 # ## Metadata
-
 # %%
-# TODO: change the path to work on microbiome not in local
-path_name = "/home/microbiome/data/0.2_piglets_metadata/"
-
+path_name = "/home/piermarco/Documents/Thesis/data/0.2_piglets_metadata/"
+# path_name = "/home/microbiome/data/0.2_piglets_metadata/"
 metadta_file = os.path.join(path_name, "piglets_metadata.tsv")
-
 metadata = pd.read_csv(metadta_file, sep="\t")
 metadata = pd.DataFrame(metadata)
+print(metadata.head())
 
 
-# %%
 def save_metadata(metadata, metadata_file_name):
+    """
+    Saves the metadata dataframe to a file.
+
+    Args:
+    metadata (pandas.DataFrame): The metadata dataframe.
+    meradata_file_name (str): The name of the metadata file.
+
+    Returns:
+    None
+    """
     new_path_name = path_name
     if not os.path.exists(new_path_name):
         os.makedirs(new_path_name)
@@ -27,33 +44,84 @@ def save_metadata(metadata, metadata_file_name):
     )
 
 
+def generate_correlation_matrix(metadata):
+    """
+    Generates and plots the correlation matrix for the numeric columns in the metadata.
+
+    Args:
+    metadata (pandas.DataFrame): The metadata dataframe.
+
+    Returns:
+    None
+    """
+    is_numeric = metadata.iloc[0] == "numeric"
+    numeric_columns = metadata.columns[is_numeric]
+    numeric_data = metadata[numeric_columns].iloc[1:]
+    numeric_data = numeric_data.apply(pd.to_numeric, errors="coerce")
+
+    corr_matrix = numeric_data.corr()
+
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(corr_matrix, annot=True, cmap="coolwarm")
+    plt.title("Correlation Matrix")
+    plt.show()
+
+
+def perform_pca(metadata):
+    """
+    Performs Principal Component Analysis (PCA) on the numeric columns of the metadata.
+
+    Args:
+    metadata (pandas.DataFrame): The metadata dataframe.
+
+    Returns:
+    None
+    """
+    is_numeric = metadata.iloc[0] == "numeric"
+    numeric_columns = metadata.columns[is_numeric]
+    numeric_data = metadata[numeric_columns].iloc[1:]
+    numeric_data = numeric_data.apply(pd.to_numeric, errors="coerce")
+
+    scaler = StandardScaler()
+    scaled_data = scaler.fit_transform(numeric_data.select_dtypes(include=[np.number]))
+
+    pca = PCA(n_components=2)  # You can change the number of components
+    pca_result = pca.fit_transform(scaled_data)
+
+    plt.figure(figsize=(8, 6))
+    plt.scatter(pca_result[:, 0], pca_result[:, 1], alpha=0.7)
+    plt.xlabel("Principal Component 1")
+    plt.ylabel("Principal Component 2")
+    plt.title("PCA of Dataset")
+    plt.show()
+
+    print("Variance explained by each component:", pca.explained_variance_ratio_)
+
+
+generate_correlation_matrix(metadata)
+perform_pca(metadata)
+
+
 # %% [markdown]
-# ### 1) Modification metadata
+# ### Modification of ANIMAL_ID
 #
-# Add the time column [time][numerical]{0, 1, 2} --> correspoding to the time in [Animal ID] [categorical]{T0, T1, T2}
+# Add the time column [time][numerical]{0, 1, 2} --> correspoding to the time in [Animal ID]{T0, T1, T2}
 #
-# Add the sow column [sow][numerical]{1, 2, 3, ..., 10} --> correspoding to the sow in [Animal ID] [categorical]{S0, S1, S2, ..., S10}
+# Add the sow column [sow][numerical]{1, 2, 3, ..., 10} --> correspoding to the sow in [Animal ID]{S0, S1, S2, ..., S10}
 #
-# Add the sow_son column [sow_son][numerical]{10, 11, 12, ..., 100} --> correspoding to the sow in [Animal ID] [categorical]{S1_P0, S1_P1, S2_P2, ..., S10_P0}
-
-
+# Add the sow_son column [sow_son][numerical]{10, 11, 12, ..., 100} --> correspoding to the sow in [Animal ID]{S1_P0, S1_P1, S2_P2, ..., S10_P0}
+#
+#
 # %%
 metadata_animalID_sow = metadata[["Animal ID", "sow"]]
-
 metadata_time = metadata_animalID_sow["Animal ID"].copy()
 metadata_sow_mother_son = metadata["Animal ID"].copy()
 metadata_sow_mother = metadata["Animal ID"].copy()
 
-# %% [markdown]
-# ### Change the type of the columns from categorical to numerical
-
-# %%
 metadata_sow_mother_son[0] = "numeric"
 metadata_sow_mother[0] = "numeric"
-metadata_time[0] = "categorical"
-# %% [markdown]
-# ### Modify the values of the columns
-# %%
+metadata_time[0] = "numeric"
+
 for sow in metadata_animalID_sow["Animal ID"][1:]:
     sow_mother = (sow.split("_")[0]).replace("S", "")
     int(sow_mother)
@@ -65,20 +133,20 @@ for sow in metadata_animalID_sow["Animal ID"][1:]:
     int(sow_son)
     metadata_sow_mother_son.replace(sow, sow_son, inplace=True)
 
-metadata_time[1:] = metadata_animalID_sow["Animal ID"][1:].str[-2:]
-# %% [markdown]
-# #### Add the columns to the metadata
-# %%
+metadata_time[1:] = metadata_animalID_sow["Animal ID"][1:].str[-1:]
+
 metadata["time"] = metadata_time
-# neighboorhood already has the same shape of sow_son
-# metadata["sow_son"] = metadata_sow_mother_son
+metadata["sow_child"] = metadata_sow_mother_son
 metadata["sow"] = metadata_sow_mother
 
-
+metadata.drop("Animal ID", axis=1, inplace=True)
+metadata.drop("sow", axis=1, inplace=True)
 # %% [markdown]
 #
-# ### 2)PERCENTAGE
+# ### Percentage
 # Why am I conducting this study?
+#
+# E.g.:
 #
 # I aim to investigate whether there is a correlation between
 # the number of piglets in the nest and the number of piglets alive.
@@ -96,11 +164,9 @@ metadata["sow"] = metadata_sow_mother
 
 # %%
 metadata_nest_dead = metadata[["nest", "dead"]]
-# Calculate the proportion of survived piglets for each row
 perc_dead = metadata_nest_dead[1:].apply(
     lambda x: int(x["dead"]) * 100 / int(x["nest"]), axis=1
 )
-
 mean_dead = perc_dead.mean()
 metadata_dead = metadata["swab_ID"].copy()
 for i in range(1, len(metadata_dead)):
@@ -108,7 +174,6 @@ for i in range(1, len(metadata_dead)):
         metadata_dead[i] = "high-survivability"
     else:
         metadata_dead[i] = "low-survivability"
-
 metadata["survivability"] = metadata_dead
 
 
@@ -119,22 +184,53 @@ metadata["survivability"] = metadata_dead
 
 # %%
 metadata_nest_convert_int = metadata["nest"][1:].apply(lambda x: int(x))
-
 metadata_nest_mean = metadata_nest_convert_int.mean()
-
-
 metadata_nest = metadata["swab_ID"].copy()
-
 for i in range(1, len(metadata_nest)):
     if metadata_nest_convert_int[i] < metadata_nest_mean:
         metadata_nest[i] = "small-nest"
     else:
         metadata_nest[i] = "big-nest"
-
-
 metadata["nest_size"] = metadata_nest
+
+
 # %% [markdown]
-# ### ARISING PROBLEM (Inconsistent Data)
+#
+# #### Study on the Number of Piglets Eliminated Due to Underweight
+#
+# I created a proportion: $ \frac{nest}{100} = \frac{underweight}{underweight_{perc}}$. Then, I divided the piglets into two groups: underweight and not underweight. In this case, I only considered piglets after time $T_0$.
+
+# %%
+metadata_nest_underweight = metadata[["nest", "uw_el"]]
+
+perc_dead = metadata_nest_underweight[1:].apply(
+    lambda x: (int(x["uw_el"]) * 100 / (int(x["nest"]))), axis=1
+)
+mean_dead = perc_dead.mean()
+metadata_dead = metadata["swab_ID"].copy()
+
+for i in range(1, len(metadata_dead)):
+    if perc_dead[i] < mean_dead:
+        metadata_dead[i] = "UW_nest_size"
+    else:
+        metadata_dead[i] = "not_UW_nest_size"
+metadata["UW_nest_size"] = metadata_dead
+
+# %% [markdown]
+# #### Study on the number of piglets transferred
+#
+# Just bring to 0 the transferred piglets aftere time T0
+# %%
+metadata_transferred = metadata["transferred"].copy()
+metadata_transferred_time = metadata[["time", "transferred"]]
+metadata_transferred[1:] = metadata_transferred_time[1:].apply(
+    lambda x: 0 if x["time"] != "0" else x["transferred"], axis=1
+)
+metadata["transferred_time"] = metadata_transferred
+save_metadata(metadata, "piglets_metadata_new.tsv")
+
+# %% [markdown]
+# ### ARISING PROBLEM (Inconsistent Data?)
 #
 # There are five columns strictly correlated with the number of piglets:
 #
@@ -147,114 +243,43 @@ metadata["nest_size"] = metadata_nest
 # 4. **uw_el:** Number of piglets underweight.
 #
 # 5. **transferred:** Number of piglets transferred T0.
-#
-# However, we can easily notice that the first three columns are correlated with time $T_0$.
-# Therefore, the equation is defined as follows: ${nest} = {alive} + {dead}$.
-#
-# But also the number transferred are correlated with time $T_0$.
-#
-# Instead, we don't know at which time ${uw_el}$ is referred. Could it be piglets born underweight at time $T_0$? Or could they become underweight at time $T_1$ or later?
-#
-# However, the equations are defined as follows:
-#
-# - For the new number of alive piglets: `new_alive = alive - uw_el`.
-#
-#
-# The problem arises when these equations are not respected. For example, we can have the following situation:
-#
-# Respected time T0:
-#
-# | nest      | alive    | dead   | transferred | uw_el  | time   |
-# |-----------|----------|--------|-------------|--------|--------|
-# | numeric   | numeric  | numeric| numeric     | numeric|category|
-# | 23        | 19       | 4      | 2           | 2      |T0      |
-#
-# Not respected time T1:
-#
-# | nest      | alive    | dead   | transferred | uw_el  | time   |
-# |-----------|----------|--------|-------------|--------|--------|
-# | numeric   | numeric  | numeric| numeric     | numeric|category|
-# | 23        | 19       | 4      | 2           | 2      |T1      |
-#
-# To be consistent, it should be like this (nest, for me, should be modified with alive due to the fact that in metadata it is specified that alive is the number of piglets alive in the nest at birth, so time T0, and the ones transferred at time T0):
-#
-# | nest      | alive    | dead   | transferred | uw_el  | time   |
-# |-----------|----------|--------|-------------|--------|--------|
-# | numeric   | numeric  | numeric| numeric     | numeric|category|
-# | 17        | 19       | 4      | 2           | 2      |T0      |
-#
+
 # %% [markdown]
-
-# #### Study on the Number of Piglets Eliminated Due to Underweight
-
-# I created a proportion: $ \frac{nest_{piglets}}{100} = \frac{underweight_{piglets}}{underweight_{percentage}}$. Then, I divided the piglets into two groups: underweight and not underweight. In this case, I only considered piglets after time $T_0$.
-
+# #### Study over the time
+# -[x] We don't know when piglets die
+#
+# -[x] We don't know when piglets are transferred based on metadata provided we suppoese time t0
+#
+# -[x] We don't know when piglets are underweight
+#
+# -[x] We know at the end how many pigles are alive but not at the correct time
+#
+# Based on some check the coorect formula seems to be the following one:
+# $$ {alive}_{T0} = {nest}_{T0} - {dead}_{T0} $$
+# $$ {weaned}_{T2} = {alive}_{T0} - {transferred}_{T1T2} - {underweight}_{T1T2} $$
 # %%
-metadata_nest_underweight = metadata[["alive", "uw_el", "time", "transferred"]]
-
-perc_dead = metadata_nest_underweight[1:].apply(
-    lambda x: (int(x["uw_el"]) * 100 / (int(x["alive"])) - int(x["transferred"]))
-    if x["time"] != "T0"
-    else 0,
-    axis=1,
+# Convert columns to numeric
+columns_to_convert = ["nest", "alive", "weaned", "dead", "transferred", "uw_el"]
+metadata_new = metadata.copy()
+metadata_new = metadata_new[1:]
+for col in columns_to_convert:
+    metadata_new[col] = pd.to_numeric(metadata[col], errors="coerce")
+check_alive = metadata_new["alive"] == metadata_new["nest"] - metadata_new["dead"]
+check_weaned = (
+    metadata_new["weaned"]
+    == metadata_new["alive"] - metadata_new["transferred"] - metadata_new["uw_el"]
 )
 
-# Calculate mean_dead
-somma = 0
-count = 0
-for i in range(1, len(perc_dead)):
-    if metadata_nest_underweight["time"][i] != "T0":
-        somma += perc_dead[i]
-        count += 1
+for check in check_alive:
+    if check != True:
+        print("miss equation in check_alive")
 
-mean_dead = somma / count
-
-# Modify metadata_dead based on mean_dead
-metadata_dead = metadata["swab_ID"].copy()
-
-for i in range(1, len(metadata_dead)):
-    if perc_dead[i] < mean_dead and metadata_nest_underweight["time"][i] != "T0":
-        metadata_dead[i] = "lot-of-underweight"
-    elif metadata_nest_underweight["time"][i] != "T0":
-        metadata_dead[i] = "few-underweight"
-    else:
-        metadata_dead[i] = "none"
-
-metadata["underweight"] = metadata_dead
+for check in check_weaned:
+    if check != True:
+        print("miss equation in check_weaned")
 
 # %% [markdown]
-# #### Study on the number of piglets transferred
-# Just bring to 0 the transferred piglets aftere time T0
+# ### Drop and rename column and save the new metadata file
 # %%
-metadata_transferred = metadata["transferred"].copy()
-metadata_transferred_time = metadata[["time", "transferred"]]
-
-metadata_transferred[1:] = metadata_transferred_time[1:].apply(
-    lambda x: 0 if x["time"] != "T0" else x["transferred"], axis=1
-)
-
-metadata["transferred"] = metadata_transferred
-
-# %% [markdown]
-# ### Drop uselss column by checking the correlation between some of the columns
-# - **rename** the $neigh$ column to $cell$.
-# - **Drop** $Animal \space ID$ defined as categorical split in $time$ and $sow$ and $sow_{son}$ the last one can also can be omitted due to the fact
-# correspond as the same as the $cell$ column.
-# - **Drop** $dead$ and $alive$ due to the fact that they are correlated with $nest$ and they seems redundant better usage of means and percentage as created before.
-# - **Drop** $swab_{ID}$ due to the fact that it is a unique identifier and it is not useful for the analysis and also are present others unique identifier.
-# - **Drop** $nest$ due to the fact that it is correlated with $dead$ and $alive$, and we calculate the difference for a big or small nest.
-
-# %%
-metadata.drop("Animal ID", axis=1, inplace=True)
 metadata.drop("swab_ID", axis=1, inplace=True)
 metadata.rename(columns={"neigh": "cell"}, inplace=True)
-save_metadata(metadata, "piglets_metadata_modified_full1.tsv")
-metadata.drop("dead", axis=1, inplace=True)
-metadata.drop("nest", axis=1, inplace=True)
-metadata.drop("alive", axis=1, inplace=True)
-save_metadata(metadata, "piglets_metadata_modified_neglet_some2.tsv")
-
-# %% [markdown]
-# ### Save the metadata
-
-# %%

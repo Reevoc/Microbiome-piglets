aa1ed7189574cddd910e395ac7387ef1924e1565
Reevoc <61749102+Reevoc@users.noreply.github.com>
2024-01-08 16:38:59 +0100

    DA_MasLin2 update

--- docker_app/src/sh/diff_abb.sh
+++ docker_app/src/sh/diff_abb.sh
@@ -3,35 +3,29 @@
 cd /home/microbiome/
 
 echo "START script for the computation of taxa barplot"
-# Column ANCOM should be categorical
+
+
 if [ $# -ne 4 ]; then
     echo "Usage: $0 <colum_name> <normalization> <metadata> <quantile>"
     exit 1
 fi
 
-# Activate the Qiime2 environment
-# Ensure that this works as expected in your setup
 source activate microbiome
 
 list_table=("asv" "genus" "species")
 list_number=("1" "2" "3")
-
-# Assuming the length of both lists is the same
 
 for i in "${!list_table[@]}"; do
     echo "table: ${list_table[$i]}"
     table=${list_table[$i]}
     number=${list_number[$i]}
 
-    # Run the Python script and capture the output
     output=$(python /home/microbiome/docker_app/src/py/extract_csv.py ${table} $2 $4)
     read sample_frequency feature_frequency <<< "$output"
 
-    # Check and set minimum frequency if zero
     [ "$sample_frequency" -eq 0 ] && sample_frequency=1
     [ "$feature_frequency" -eq 0 ] && feature_frequency=1
 
-    # Use sample_frequency and feature_frequency in your script
     echo "Sample Frequency: $sample_frequency"
     echo "Feature Frequency: $feature_frequency"
 
@@ -56,7 +50,6 @@ for i in "${!list_table[@]}"; do
         --o-composition-table "data/13.${number}_${table}_$2_DA_ANCOM/${table}_$2_DA_ANCOM_composition.qza"
     echo "Adding pseudocount --> done"
 
-    # create a qzv for the last file before lunching ancom 
     qiime feature-table summarize \
         --i-table "data/13.${number}_${table}_$2_DA_ANCOM/${table}_$2_sample_feature_filtered.qza" \
         --o-visualization "data/13.${number}_${table}_$2_DA_ANCOM/${table}_$2_DA_ANCOM_composition.qzv" \
